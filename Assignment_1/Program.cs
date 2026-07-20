51c8ee25cb57301a3816bfd9c4def6a37af08c43
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-07-20 10:50:38 +0300

    Question 1

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -2,8 +2,31 @@
 
 class Program
 {
+    /*
+    The four universal methods
+        to_string (),GetType()
+        Equle()->check the reference of two objects && if variables are of value type it will check the value of two variables
+        getHashCode()-> help in direct access in map & hashtables
+        if(equles)true then HashCodeIsTheSame, Must override if Equals is overridden. 
+
+    Convert String To number
+    Parse()       ,   TryParse()    
+    throw Exption ,  return false
+     */
+
+    class Book
+    {
+        string Title;
+        int Pages;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("ahmed Esmail");
+        #region Question 1
+            // //Create a Book class with a Title (string) and Pages (int).
+            // //Create a Book object and store it in a variable of type object. Print it.
+            // object book =new Book();
+            // Console.WriteLine(book);
+        #endregion
+        
     }
 }
--- Assignment_1/obj/Debug/net10.0/Assignment_1.AssemblyInfo.cs
+++ Assignment_1/obj/Debug/net10.0/Assignment_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e821bf40ccb25f06eaf9df3ff4dfdeea71ef139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
