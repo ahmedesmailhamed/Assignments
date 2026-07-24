c386b432a0467cf1c6ae55df8ac413491e428ada
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-07-24 11:14:54 +0300

    fix gitignore

--- Assignment_2/obj/Debug/net10.0/Assignment_2.AssemblyInfo.cs
+++ Assignment_2/obj/Debug/net10.0/Assignment_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fecf57ac04153789cad0480c3aa4f56232fb4772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db30245542e8ef2446e786079d008c3336b4f10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
