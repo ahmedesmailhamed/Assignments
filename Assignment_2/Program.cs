fecf57ac04153789cad0480c3aa4f56232fb4772
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-07-24 10:54:35 +0300

    Question 1

---
+++ Assignment_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Qusetion1
+        //         Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print
+// both title and upperTitle to show that title did not change.
+            string title="clean code";
+            string upperTitle=title.ToUpper();
+            System.Console.WriteLine(title);
+            System.Console.WriteLine(upperTitle);            
+        #endregion
+
+    }
+}
