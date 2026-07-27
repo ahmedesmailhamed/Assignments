6b0595ce30583c513a3630a78fcb24f41d5c0fe1
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-07-27 22:42:04 +0300

    Q1

---
+++ Assignment_3/Assignment_3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Q1
+           double []prices={25.5, 40.0, 33.75};
+           System.Console.WriteLine(prices[1]);
+        #endregion
+    }
+}
