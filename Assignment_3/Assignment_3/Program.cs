da5c600816a903841fb4e82bd7c2128d7fdf1e9a
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-07-27 22:47:13 +0300

    Q2

--- Assignment_3/Assignment_3/Program.cs
+++ Assignment_3/Assignment_3/Program.cs
@@ -8,5 +8,11 @@ class Program
            double []prices={25.5, 40.0, 33.75};
            System.Console.WriteLine(prices[1]);
         #endregion
+        #region Q2
+//             // Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+// shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+            int [,] shelfCopies={{3,5},{1,4}};
+            System.Console.WriteLine(shelfCopies[1,0]);
+        #endregion
     }
 }
