e37ae0e81300c25ca60f499be1d33690f1fd81c4
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-07-27 23:50:41 +0300

    Q10

--- Assignment_3/Assignment_3/Program.cs
+++ Assignment_3/Assignment_3/Program.cs
@@ -35,6 +35,10 @@ class Program
         price=0;
         return false;
     }
+    static void PrintBookInfo(string title, int pages = 300)
+    {
+        System.Console.WriteLine(pages);
+    }
     static void Main(string[] args)
     {
         #region Q1
@@ -97,9 +101,17 @@ class Program
         #region Q9
 //             Write a method bool TryGetPrice(string title, out double price) that returns true and sets
 // price to 25.5 if title is "Clean Code", otherwise returns false and sets price to 0. Call itand print the price if found
-                 string title="Clean Code";
-                 TryGetPrice( title, out double price);
-                 System.Console.WriteLine(price);
+                //  string title="Clean Code";
+                //  TryGetPrice( title, out double price);
+                //  System.Console.WriteLine(price);
         #endregion
+        #region Q10
+//             Write a method PrintBookInfo(string title, int pages = 300) where pages is optional. Call
+// it once with only a title, and once passing both a title and pages
+            string title="Clean Code";
+                PrintBookInfo( title,   500);
+                PrintBookInfo( title);
+        #endregion
+
     }
 }
