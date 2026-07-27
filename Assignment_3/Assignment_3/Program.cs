c97ee7db975673f1d72f3dc822e86d069e31df7e
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-07-27 22:55:05 +0300

    Q3

--- Assignment_3/Assignment_3/Program.cs
+++ Assignment_3/Assignment_3/Program.cs
@@ -2,17 +2,27 @@
 
 class Program
 {
+    static void PrintWelcomeMessage()
+    {
+        System.Console.WriteLine("Welcome to the Library!");
+    }
     static void Main(string[] args)
     {
         #region Q1
-           double []prices={25.5, 40.0, 33.75};
-           System.Console.WriteLine(prices[1]);
+        //    double []prices={25.5, 40.0, 33.75};
+        //    System.Console.WriteLine(prices[1]);
         #endregion
         #region Q2
 //             // Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
 // shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
-            int [,] shelfCopies={{3,5},{1,4}};
-            System.Console.WriteLine(shelfCopies[1,0]);
+            // int [,] shelfCopies={{3,5},
+            //                      {1,4}};
+            // System.Console.WriteLine(shelfCopies[1,0]);
+        #endregion
+        #region Q3
+//         Write a method called PrintWelcomeMessage that takes no parameters and prints
+// "Welcome to the Library!". Call it from Main.
+            PrintWelcomeMessage();
         #endregion
     }
 }
