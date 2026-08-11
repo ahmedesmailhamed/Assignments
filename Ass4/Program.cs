b5efefbca4e9a755ca197fe0a6e3daf0c825b3ec
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-08-11 15:28:32 +0300

    Q1

---
+++ Ass4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ass4;
+class Book
+{
+    private string password="secret";
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Add a private string password = "secret";
+        //  field to a Book class. Try to print it from Main (outside the class). What happens, and why?
+        Book book=new Book();
+        System.Console.WriteLine(book.password);
+        //compile erroe ,private mean the field is avaliable in the class only
+    }
+}
