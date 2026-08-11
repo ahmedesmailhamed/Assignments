0914ce6f3de291c7990b4cb0f71de2d7d83eeb14
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-08-11 15:39:10 +0300

    Q2

--- Ass4/Program.cs
+++ Ass4/Program.cs
@@ -2,15 +2,28 @@
 class Book
 {
     private string password="secret";
+    internal int copiesInStock = 5;
 }
 class Program
 {
     static void Main(string[] args)
     {
-        // Add a private string password = "secret";
+        #region Q1
+            //Q1: Add a private string password = "secret";
         //  field to a Book class. Try to print it from Main (outside the class). What happens, and why?
         Book book=new Book();
-        System.Console.WriteLine(book.password);
+        //System.Console.WriteLine(book.password);
         //compile erroe ,private mean the field is avaliable in the class only
+        #endregion
+        
+        #region Q2
+            //Q2
+        //Add an internal int copiesInStock = 5;
+        //  field to Book. Print it from Main. Does it compile?Why?
+       System.Console.WriteLine(book.copiesInStock);
+       //it's compiled because internal can be accessed in same project (the same assembly code)
+
+        #endregion
+        
     }
 }
