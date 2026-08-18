7b63cfff9cf46c2cd7c65a12199ac32bdea9f400
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-08-18 15:43:00 +0300

    oop_1

---
+++ OOP/OOP_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Q1
+        //a)
+//      DeliveryAddress is a struct, which is a value type.
+//      When a DeliveryAddress variable is copied into another variable, a separate copy is created.
+//      So, if we modify the copied variable, the original variable will not be affected.
+
+        //b)
+        /*
+        Customer is a class, which is a reference type.
+        When a Customer variable is copied into another variable, both variables refer to the same object.
+        Therefore, if one variable modifies the object, the changes will also be visible through the other variable.
+        */
+         #endregion
+    }
+}
