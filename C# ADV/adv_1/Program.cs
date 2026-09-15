2b9fc510f51380c9774d34aa05721729c99d3b65
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-09-15 13:22:34 +0300

    Q1

---
+++ C# ADV/adv_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace adv_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Q1
+            // a) What is a Generic Class?
+                // A class that can work with different data types using a type parameter such as <T>
+
+            // b) Why use Generics?
+                // To write reusable and type-safe code without repeating the same code for different data types
+        #endregion
+    }
+}
