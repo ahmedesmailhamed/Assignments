d9dcd55d8945fd0817e32df165ce619a0b0ddf29
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-09-15 14:42:42 +0300

    Q14

--- C# ADV/adv_1/Program.cs
+++ C# ADV/adv_1/Program.cs
@@ -168,5 +168,26 @@ class Program
                 // For reference types it is null
         #endregion
 
+        #region Q14
+            // Write a SafeList<T> that returns default when the index is invalid
+            class SafeList<T>
+            {
+                private List<T> items = new List<T>();
+
+                public void Add(T item)
+                {
+                    items.Add(item);
+                }
+
+                public T Get(int index)
+                {
+                    if (index < 0 || index >= items.Count)
+                        return default;
+
+                    return items[index];
+                }
+            }
+        #endregion
+
     }
 }
