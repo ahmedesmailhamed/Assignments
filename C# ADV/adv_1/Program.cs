1d9d503822ad50c9d41b21898165f81ba03a6003
ahmed.esmail.hamed@gmail.com <ahmed.esmail.hamed@gmail.com>
2026-09-15 13:36:56 +0300

    Q5

--- C# ADV/adv_1/Program.cs
+++ C# ADV/adv_1/Program.cs
@@ -60,5 +60,16 @@ class Program
             }
         #endregion
 
+        #region Q5
+            // a) Write a generic method FindMax<T>
+                // A generic method that finds the maximum value between elements
+
+            static T FindMax<T>(T a, T b) where T : IComparable<T>
+            {
+                return a.CompareTo(b) > 0 ? a : b;
+            }
+        #endregion
+
+
     }
 }
